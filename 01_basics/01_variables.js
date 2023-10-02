7a422942462d25b7a8aa5b37aa6edf2fe2210401
SHUBHAM AGARWAL <69917583+Shubhag7@users.noreply.github.com>
2023-10-02 16:56:11 +0000

    update variable

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,15 +1,15 @@
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+let accountEmail = "shubham@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Jhansi"
 let accountState;
 
 // accountId = 2 // not allowed
 
 
-accountEmail = "hc@hc.com"
+accountEmail = "sa@sa.com"
 accountPassword = "21212121"
-accountCity = "Bengaluru"
+accountCity = "Delhi"
 
 console.log(accountId);
 
